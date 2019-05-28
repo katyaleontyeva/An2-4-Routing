d44da2a9aa7168e79a97cd46bcaf772f3e7db418
Ekaterina Leonteva <ekaterina_leonteva@epam.com>
2019-05-28 23:02:19 +0300

    Task 21

---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { CoreModule } from '../core.module';
+
+@Injectable({
+  providedIn: CoreModule
+})
+export class AuthGuard implements CanActivate {
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    console.log('CanActivateGuard is called');
+    return true;
+  }
+}
