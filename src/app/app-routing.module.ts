e9db2d18b6cf486631ba9c539069094bbe032302
Ekaterina Leonteva <ekaterina_leonteva@epam.com>
2019-05-29 00:39:58 +0300

    Task 32

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules, ExtraOptions } from '@angular/router';
 
 import { AboutComponent, PathNotFoundComponent, MessagesComponent, LoginComponent } from './layout';
-import { AuthGuard } from './core';
+import { AuthGuard, CustomPreloadingStrategyService } from './core';
 
 const routes: Routes = [
   {
@@ -30,7 +30,8 @@ const routes: Routes = [
   },
   {
     path: 'users',
-    loadChildren: './users/users.module#UsersModule'
+    loadChildren: './users/users.module#UsersModule',
+    data: { preload: true }
   },
   {
     // The router will match this route if the URL requested
@@ -41,7 +42,8 @@ const routes: Routes = [
 ];
 
 const extraOptions: ExtraOptions = {
-  preloadingStrategy: PreloadAllModules,
+  // preloadingStrategy: PreloadAllModules,
+  preloadingStrategy: CustomPreloadingStrategyService,
   enableTracing: true // Makes the router log all its internal events to the console.
 };
 
--- src/app/core/index.ts
+++ src/app/core/index.ts
@@ -2,6 +2,7 @@ export * from './services/messages.service';
 export * from './services/auth.service';
 export * from './services/dialog.service';
 export * from './services/spinner.service';
+export * from './services/custom-preloading-strategy.service';
 
 export * from './interfaces/can-component-deactivate.interface';
 
